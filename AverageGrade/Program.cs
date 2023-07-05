2b2cc9296f21890b26976a3a6d0559c8799fe3ee
TypxaH <turhan@abv.bg>
2023-07-05 21:06:56 +0300

    Add project files.

---
+++ AverageGrade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AverageGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            StreamReader reader = new StreamReader("students.txt");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                List<string> studentInfo = line
+                    .Split(',')
+                    .ToList();
+                Student current = new Student(studentInfo[0], int.Parse(studentInfo[1]), int.Parse(studentInfo[2]), int.Parse(studentInfo[3]));
+               
+                students.Add(current);
+
+                line = reader.ReadLine();
+            }
+            StreamWriter writer = new StreamWriter("output.txt");
+            using (writer)
+            {
+                foreach (Student st in students)
+                {
+                    double avarege = (st.BelGrade + st.MatGrade + st.InfoGrade) / 3.0;                    
+                    writer.WriteLine($"{st.Name} - {avarege:f2}");
+                }
+            }
+        }
+    }
+}
---
+++ AverageGrade/Student.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace AverageGrade
+{
+    class Student
+    {
+        private string name;
+        private int belGrade;
+        private int matGrade;
+        private int infoGrade;
+
+        public Student(string name, int belGrade, int matGrade, int infoGrade)
+        {
+            this.name = name;
+            this.belGrade = belGrade;
+            this.matGrade = matGrade;
+            this.infoGrade = infoGrade;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int BelGrade { get => belGrade; set => belGrade = value; }
+        public int MatGrade { get => matGrade; set => matGrade = value; }
+        public int InfoGrade { get => infoGrade; set => infoGrade = value; }
+    }
+}
